0d728fbc1b201b0b559b82b5c2344e053d6523bf
Vladimir Matskevich <freegun.super@yandex.ru>
2023-01-21 22:52:34 +0500

    переписал верстку и стили

--- script/card.js
+++ script/card.js
@@ -8,13 +8,23 @@ class Card {
     const template = document.querySelector(this._selector).content;
     const card = template.querySelector(".card").cloneNode(true);
     const cardImage = card.querySelector(".card__image");
-    const cardTitle = card.querySelector(".card__name");
-    const cardLike = card.querySelector(".card__like");
+    const cardTitle = card.querySelector(".card__title");
+    const cardLike = card.querySelector(".card__footer-like");
+    const cardRating = card.querySelector(".card__footer-rating");
 
     cardImage.src = this._data.image;
     cardTitle.textContent = this._data.name;
     if (!this._data.favorite) {
       cardLike.remove();
+    }
+    for (let i = 0; i < 10; i++) {
+      let star = document.createElement("i");
+      if (i < +this._data.rate) {
+        star.classList.add("fa-solid", "fa-star");
+      } else {
+        star.classList.add("fa-regular", "fa-star");
+      }
+      cardRating.append(star);
     }
     return card;
   }
--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 const mainCardsContainer = document.querySelector(".cards");
-const popupBtn = document.querySelector("#add");
-const popupCloseBtn = document.querySelector(".popup__close");
-const popup = new Popup(".popup-add-cats");
+// const popupBtn = document.querySelector("#add");
+// const popupCloseBtn = document.querySelector(".popup__close");
+// const popup = new Popup(".popup-add-cats");
 
-console.log(popup);
+// console.log(popup);
 
 // выдача карточек
 cats.forEach((cat) => {
@@ -17,13 +17,13 @@ cats.forEach((cat) => {
 // если просто передать popup.open,
 // то у нас теряется this
 // (потому что передается ссылка на функцию)
-popupBtn.addEventListener("click", () => {
-  popup.open();
-});
+// popupBtn.addEventListener("click", () => {
+//   popup.open();
+// });
 
 // можно и так, но громоздко
 // popupBtn.addEventListener("click", popup.open.bind(popup));
 
-popupCloseBtn.addEventListener("click", () => {
-  popup.close();
-});
+// popupCloseBtn.addEventListener("click", () => {
+//   popup.close();
+// });
